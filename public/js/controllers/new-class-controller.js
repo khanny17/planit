41634e5a0871ed0d9eb221793a59422245139fc0
Kevin Hannigan <khanny17@users.noreply.github.com>
2016-11-09 13:59:32 -0500

    Update new-class-controller.js

--- public/js/controllers/new-class-controller.js
+++ public/js/controllers/new-class-controller.js
@@ -2,8 +2,8 @@ angular.module('PlanitApp')
 
 .controller('newClassController', ['$scope', function($scope){
   	var defaultTime = new Date();
-  	d.setHours( 14 );
-  	d.setMinutes( 0 );
+  	defaultTime.setHours( 14 );
+  	defaultTime.setMinutes( 0 );
 
 	$scope.monday = false;
   	$scope.mondayTime = defaultTime;
