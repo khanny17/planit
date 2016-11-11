588e00eec67bf9a531b4d0b803939afd90a6404e
wpe2632 <wpe2632@mail.rit.edu>
2016-11-11 12:33:43 -0500

    Added Complete, Edit, Delete buttons for home page assignments

--- public/js/controllers/home-controller.js
+++ public/js/controllers/home-controller.js
@@ -2,8 +2,23 @@ angular.module('PlanitApp')
 
 .controller('homeController', ['$scope', 'assignmentService', function($scope, assignmentService){
     $scope.assignments = assignmentService.get();
+    $scope.completedAssignments = assignmentService.getCompleted();
 
     $scope.go = function(assignment) {
+        alert("GO!");
+    };
 
+    $scope.complete = function(assignment) {
+        //$scope.$apply(function(assignment) {
+            assignmentService.complete(assignment);
+            $scope.completedAssignments = assignmentService.getCompleted();
+        //});
+    };
+
+    $scope.incomplete = function(assignment) {
+        //$scope.$apply(function(assignment) {
+            assignmentService.incomplete(assignment);
+            $scope.assignments = assignmentService.get();
+        //});
     };
 }]);
--- public/js/services/assignment-service.js
+++ public/js/services/assignment-service.js
@@ -3,6 +3,7 @@ angular.module('PlanitApp')
     .service('assignmentService', [function(){
         var self = this;
         self.assignments = [];
+        self.completed = [];
 
         self.add = function(name, dueDate) {
             self.assignments.push({
@@ -15,4 +16,23 @@ angular.module('PlanitApp')
             return self.assignments;
         };
 
+        self.complete = function(assignment) {
+            var index = self.assignments.indexOf(assignment);
+            if (index > -1) {
+                self.assignments.splice(index, 1);
+            }
+            self.completed.push(assignment);
+        };
+
+        self.incomplete = function(assignment) {
+            var index = self.completed.indexOf(assignment);
+            if (index > -1) {
+                self.completed.splice(index, 1);
+            }
+            self.assignments.push(assignment);
+        };
+
+        self.getCompleted = function() {
+            return self.completed;
+        };
     }]);
