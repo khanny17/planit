e6fbb73dae369046c9fa0f624e7fe2195f0d5d80
Kevin Hannigan <kph1958@rit.edu>
2016-11-11 11:09:26 -0500

    wow im so out of it

--- public/js/controllers/new-assignment-controller.js
+++ public/js/controllers/new-assignment-controller.js
@@ -3,12 +3,10 @@ angular.module('PlanitApp')
     .controller('newAssignmentController', ['$scope', 'assignmentService', 'classService', '$uibModal', '$state', 
         function($scope, assignmentService, classService, $uibModal, $state){
 
-            $scope.assignment = {};
+            $scope.assignment = {
+                dueDate: new Date()
+            };
             $scope.classes = classService.getClasses() || [];
-
-            var defaultTime = new Date();
-            defaultTime.setHours( 14 );
-            defaultTime.setMinutes( 0 );
 
             if($scope.classes.length < 1) {
                 //TODO redirect to the new class page
@@ -22,52 +20,19 @@ angular.module('PlanitApp')
             };
 
             $scope.submit = function() {
-                assignmentService.add($scope.assignmentName, $scope.dt1);
+                assignmentService.add($scope.assignment);
                 $state.go('inside.home');
             };
 
             $scope.invalidForm = function() {
-                if(!$scope.assignmentName) {
+                if(!$scope.assignment.name) {
                     return true;	
                 }
-                if(!$scope.class) {
+                if(!$scope.assignment.class) {
                     return true;	
                 }
-                if(!$scope.description) {
+                if(!$scope.assignment.description) {
                     return true;	
                 }
             };
-
-            $scope.days = [
-                {
-                    name: 'Monday',
-                    active: false,
-                    time: defaultTime,
-                    duration: 60 //How long the class is, in minutes
-                },
-                {
-                    name: 'Tuesday',
-                    active: false,
-                    time: defaultTime,
-                    duration: 60
-                },
-                {
-                    name: 'Wednesday',
-                    active: false,
-                    time: defaultTime,
-                    duration: 60
-                },
-                {
-                    name: 'Thursday',
-                    active: false,
-                    time: defaultTime,
-                    duration: 60
-                },
-                {
-                    name: 'Friday',
-                    active: false,
-                    time: defaultTime,
-                    duration: 60
-                }
-            ];
         }]);
