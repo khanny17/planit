70c827c7a282c6d5c4affc69fbd17fe82f8176d3
wpe2632 <wpe2632@mail.rit.edu>
2016-11-20 17:26:04 -0500

    Fixed complete bug

--- public/js/controllers/home-controller.js
+++ public/js/controllers/home-controller.js
@@ -10,15 +10,19 @@ angular.module('PlanitApp')
 
     $scope.complete = function(assignment) {
         assignmentService.complete(assignment);
+        $scope.assignments = assignmentService.get();
         $scope.completedAssignments = assignmentService.getCompleted();
     };
 
     $scope.incomplete = function(assignment) {
         assignmentService.incomplete(assignment);
         $scope.assignments = assignmentService.get();
+        $scope.completedAssignments = assignmentService.getCompleted();
     };
 
     $scope.remove = function(assignment) {
         assignmentService.delete(assignment);
+        $scope.assignments = assignmentService.get();
+        $scope.completedAssignments = assignmentService.getCompleted();
     };
 }]);
