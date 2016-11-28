9f06e20367fbc16a56598b825fe96a131fd70f94
Kevin Hannigan <khanny17@users.noreply.github.com>
2016-11-28 14:43:11 -0500

    adding start/end times

--- public/js/controllers/account-settings-controller.js
+++ public/js/controllers/account-settings-controller.js
@@ -2,6 +2,15 @@ angular.module('PlanitApp')
 
 .controller('accountSettingsController', ['$scope', function($scope){
 
+    $scope.schoolStartTime = new Date();
+  	$scope.schoolStartTime.setHours( 8 );
+  	$scope.schoolStartTime.setMinutes( 45 );
+    
+    $scope.schoolEndTime = new Date();
+  	$scope.schoolEndTime.setHours( 14 );
+  	$scope.schoolEndTime.setMinutes( 15 );
+    
+    
     $scope.submit = function() {
         
     };
